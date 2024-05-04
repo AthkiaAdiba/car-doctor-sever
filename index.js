70d0133d7352586493a2fa60af28e95aecf24752
thkiaAdiba <athkiaadiba@gmail.com>
2024-05-04 10:51:12 +0600

    added verify

--- index.js
+++ index.js
@@ -30,6 +30,33 @@ const client = new MongoClient(uri, {
     }
 });
 
+// own middleware
+const logger = async(req, res, next) => {
+    console.log('called', req.host, req. originalUrl)
+    next()
+}
+
+
+const verifyToken = async(req, res, next) => {
+    const token = req.cookies?.token;
+    console.log('value of token is middleware', token)
+    if(!token){
+        return res.status(401).send({message: 'not authorized'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        // err
+        if(err){
+            console.log(err)
+            return res.status(401).send({message: 'unauthorized'})
+        }
+        // if token is valid then it would be decoded
+        console.log('value is the token', decoded)
+        req.user = decoded;
+        next()
+    })
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -39,7 +66,7 @@ async function run() {
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
         // auth related api
-        app.post('/jwt', async(req, res) => {
+        app.post('/jwt', logger, async(req, res) => {
             const user = req.body;
             console.log(user)
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
@@ -53,7 +80,7 @@ async function run() {
 
 
         // services related api
-        app.get('/services', async (req, res) => {
+        app.get('/services', logger, async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
@@ -76,9 +103,14 @@ async function run() {
 
         // bookings
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', logger, verifyToken, async (req, res) => {
             console.log(req.query.email)
-            console.log('tok tok token', req.cookies.token)
+            // console.log('tok tok token', req.cookies.token)
+            console.log('user in the valid token', req.user)
+            if(req.query.email !== req.user.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            
             let query = {}
             if (req.query?.email) {
                 query = {email: req.query?.email}
